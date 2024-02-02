8fabe4aa34612ed02eb92fe78f5879ca1db40b42
Joel Fuentes <jefueljefe@gmail.com>
2024-02-01 20:45:58 -0600

    Prueba Practica #1

---
+++ Prueba_Practica#1/src/prueba_practica/pkg1/Prueba_Practica1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba_practica.pkg1;
+
+/**
+ *
+ * @author jefue
+ */
+public class Prueba_Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
